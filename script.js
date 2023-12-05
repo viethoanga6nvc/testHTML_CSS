8bec42c4bc6c29bb37b1a611d1b20b2b5c81e860
viethoanga6nvc <viethoang.a6nvc@gmail.com>
2023-12-05 13:59:03 +0700

    about2

--- script.js
+++ script.js
@@ -7,4 +7,8 @@ function loadComponent() {
   $(".page-title").load("./pagetitle.html");
   $(".about1").load("./About1.html");
   $(".proud-of").load("./ProudOf.html");
+  $(".about2").load("./About2.html");
+  $(".our-team").load("./OurTeam.html");
+  $(".benifit").load("./Benifit.html");
+  $("footer").load("./Footer.html");
 }
