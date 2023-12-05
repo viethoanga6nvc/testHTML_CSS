b977e66d6ee16be9c0da221da956ac231d33fed4
viethoanga6nvc <viethoang.a6nvc@gmail.com>
2023-12-05 11:15:07 +0700

    proud of

--- script.js
+++ script.js
@@ -6,4 +6,5 @@ function loadComponent() {
   $("header").load("./header.html");
   $(".page-title").load("./pagetitle.html");
   $(".about1").load("./About1.html");
+  $(".proud-of").load("./ProudOf.html");
 }
