a8b51aff1fac65fcc410456624ae24e20f44b9ef
viethoanga6nvc <viethoang.a6nvc@gmail.com>
2023-12-06 07:29:39 +0700

    debug

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ $(function () {
 });
 
 function loadComponent() {
-  $("header").load("./Header.html");
-  $(".page-title").load("./PageTitle.html");
+  $("header").load("./header.html");
   $(".about1").load("./About1.html");
   $(".proud-of").load("./ProudOf.html");
   $(".about2").load("./About2.html");
