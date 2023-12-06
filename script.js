70f52c5753731b0717338f1107c2029818872b4f
viethoanga6nvc <viethoang.a6nvc@gmail.com>
2023-12-06 07:21:35 +0700

    fix

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ $(function () {
 });
 
 function loadComponent() {
-  $("header").load("./header.html");
-  $(".page-title").load("./pagetitle.html");
+  $("header").load("./Header.html");
+  $(".page-title").load("./PageTitle.html");
   $(".about1").load("./About1.html");
   $(".proud-of").load("./ProudOf.html");
   $(".about2").load("./About2.html");
